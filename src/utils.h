20fa54af2f7e243cbd94cf030dbf5e8f020ac620
PXMYH <michael.yue.hu@gmail.com>
2017-04-23 23:59:49 -0400

    add message queue

--- src/utils.h
+++ src/utils.h
@@ -6,6 +6,10 @@
 #ifndef INCLUDE_UTILS_H_
 #define INCLUDE_UTILS_H_
 
+#include <sys/types.h>
+#include <sys/queue.h>
+#include <semaphore.h>
+
 /*
  * function: random_data_pkt_generator
  * generate random number of random sized data packets
@@ -19,5 +23,28 @@ int random_data_pkt_generator(char* data_pkt_buf, unsigned int * buffer_size_in_
  * return size of generated packet in Byte
  * */
 int data_pkt_generator (char* data, unsigned int buffer_size_in_bytes);
+
+/* smq_msg_destroy options */
+#define SMQ_CONTAINER_ONLY      0
+#define SMQ_DESTROY_ALL         1
+
+
+typedef struct _smq *            smq;
+
+struct smq_msg {
+	void                    *data;
+	size_t                   data_len;
+};
+
+smq              smq_create(void);
+int              smq_send(smq, struct smq_msg *);
+struct smq_msg  *smq_receive(smq);
+size_t           smq_len(smq);
+void             smq_settimeout(smq, struct timeval *);
+int              smq_dup(smq);
+int              smq_destroy(smq);
+
+struct smq_msg  *smq_msg_create(void *, size_t);
+int              smq_msg_destroy(struct smq_msg *, int);
 
 #endif /* INCLUDE_UTILS_H_ */
